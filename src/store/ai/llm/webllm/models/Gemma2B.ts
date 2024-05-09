3633a72b2b2fdacb73e1bcb499b7e238ed1aee0a
Nico Martin <mail@nico.dev>
2024-05-09 22:36:44 +0200

    switched foler for llm models

--- src/store/ai/llm/webllm/models/Gemma2B.ts
+++ src/store/ai/llm/webllm/models/Gemma2B.ts
@@ -2,10 +2,10 @@ import Model from '../Model';
 
 const model = new Model({
   title: 'Gemma-2B',
-  url: 'https://huggingface.co/mlc-ai/gemma-2b-it-q4f16_1-MLC/resolve/main/',
+  url: 'https://uploads.nico.dev/mlc-llm-libs/gemma-2b-it-q4f16_1-MLC/',
   size: 1429128056,
   libUrl:
-    'https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/gemma-2b-it/gemma-2b-it-q4f16_1-ctx1k_cs1k-webgpu.wasm',
+    'https://uploads.nico.dev/mlc-llm-libs/gemma-2b-it-q4f16_1-MLC/lib/gemma-2b-it-q4f16_1-ctx1k_cs1k-webgpu.wasm',
   requiredFeatures: ['shader-f16'],
   //vram_required_MB: 1476.52,
   //low_resource_required: true,
